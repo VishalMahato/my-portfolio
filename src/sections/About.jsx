686ddea5ea15d776d3ff71cdcbe2cfc5b777c3aa
VishalMahato <mailmevishalmahato@gmail.com>
2025-06-04 02:39:57 +0530

    added time zone in grid 3 and completed grid 2 in About me section

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import Card from "../components/Card";
+import { Globe } from "../components/globe";
 const About = () => {
+	const grid2Container = useRef();
 	return (
 		<section className="c-space section-spacing ">
 			<h2 className="text-heading">About Me</h2>
@@ -25,17 +27,69 @@ const About = () => {
 				</div>
 
 				{/* Grid2 */}
-				<div className="grid-default-color grid-2 ">
-          <div className="flex items-center justify-center w-full h-full">
-            <p className="flex items-end text-5xl  ">
-              CODE IS CRAFT
-            </p>
-            <Card/>
-          </div>
-        </div>
+				<div className="grid-default-color grid-2">
+					<div
+						ref={grid2Container}
+						className="flex items-center justify-center w-full h-full"
+					>
+						<p className="flex items-end text-5xl text-gray-500">
+							CODE IS CRAFT
+						</p>
+						<Card
+							style={{ rotate: "75deg", top: "30%", left: "20%" }}
+							text="GRASP"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+							text="SOLID"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+							text="Design Patterns"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+							text="Design Principles"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "20deg", top: "10%", left: "38%" }}
+							text="SRP"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "30deg", top: "70%", left: "70%" }}
+							image="assets/logos/csharp-pink.png"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+							image="assets/logos/dotnet-pink.png"
+							containerRef={grid2Container}
+						/>
+						<Card
+							style={{ rotate: "-45deg", top: "5%", left: "10%" }}
+							image="assets/logos/blazor-pink.png"
+							containerRef={grid2Container}
+						/>
+					</div>
+				</div>
 
 				{/* Grid3 */}
-				<div className="grid-black-color grid-3"></div>
+				<div className="grid-black-color grid-3">
+					<div className="z-10 w-[50%]">
+						<p className="headtext">Time Zone</p>
+						<p className="subtext">
+							I'm based in India, and open to remote work worldwide
+						</p>
+					</div>
+					<figure className="absolute left-[30%] top-[10%]">
+						<Globe/>
+					</figure>
+				</div>
 
 				{/* Grid4*/}
 				<div className="grid-special-color grid-4"></div>
